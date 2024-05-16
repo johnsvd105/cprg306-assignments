987740b1c7cc9f9fac546672f215f3dedaf8bfef
John <126916249+johnsvd105@users.noreply.github.com>
2024-05-16 12:02:29 -0600

    week-2 assignment
    week 2 assignment. No styling

---
+++ app/week-2/student-info.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+
+const StudentInfo = () => {
+    return (
+        <h1>
+            <p>John Sugden-van Dyk</p>
+            <Link href='https://github.com/johnsvd105/cprg306-assignments.git'>github.com/johnsvd105/cprg306-assignments </Link>
+        </h1>
+
+    );
+
+}
+
+export default StudentInfo;
+
