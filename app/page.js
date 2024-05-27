c1b0fb4a63de2b45f7124436f129cc2370f0bb8f
John <126916249+johnsvd105@users.noreply.github.com>
2024-05-26 22:04:14 -0600

    styling and metadata change
    added styling to home page & changed metadata title to Shopping List

--- app/page.js
+++ app/page.js
@@ -3,12 +3,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <header>
-       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-       <a href="/week-2">Week 2</a>
-    </header>
-  
-
-
+    <main className="flex justify-between items-center flex-col p-20 bg-black min-h-screen">
+      <div className="text-white">
+        <h1 className="text-3xl font-bold">CPRG 306: Web Development 2 - Assignments</h1>
+        <ol className="flex flex-col text-xl m-5">
+          <a className="hover:underline" href="/week-2">Week 2 Assignment</a>
+          <a className="hover:underline" href="/week-3">Week 3 Assignment </a>
+       </ol>
+      </div>
+    </main>
   );
 }
