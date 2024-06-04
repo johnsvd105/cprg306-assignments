581eb9e7b3ef0313d8d373110aa1256833682dff
john <126916249+johnSugden-vanDyk@users.noreply.github.com>
2024-06-03 18:27:30 -0600

    useState varibles set up
    Set up the useState varibles, set defaults to "" for name, quantity of 1 and category of produce

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -2,6 +2,10 @@
 import { useState } from "react"
 
 const Item = () => {
+    const [name, setName] = useState("");
+    const [quantity, setQuantity] = useState(1);
+    const [category, setCategory] = useState("produce");
+
     return (
         <main>
             
