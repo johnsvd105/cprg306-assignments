bac01f27e33f08507ec4fd4a5263b84ab90965e6
john <126916249+johnSugden-vanDyk@users.noreply.github.com>
2024-06-08 21:24:47 -0600

    item-list updated
    sorting added, rendering the items added, buttons added(not styled yet)

--- app/week-5/item-list.js
+++ app/week-5/item-list.js
@@ -5,10 +5,32 @@ import Items from "./items.json"
 
 const ItemList = () => {
 
+  const [sortBy, setSortBy] = useState("name")
+
+  if (sortBy == "name") {
+     Items.sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  if (sortBy =="category") {
+    Items.sort((a, b) => a.category.localeCompare(b.category));
+  }
+
       return (
-        <ul>
-            
-        </ul>
+        <div>
+            <button onClick={() => setSortBy("name")}>Sort by Name</button>
+            <button onClick={() => setSortBy("category")}>Sort by Category</button>
+
+            {Items.map((item,index) => (
+            <ul>
+            <Item key={index}
+              name={item.name}
+              quantity={item.quantity}
+              category={item.category}
+            />
+            </ul>
+        ))}
+        </div>
       )
+      
 }
 export default ItemList
