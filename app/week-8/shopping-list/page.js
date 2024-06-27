1cb49ca2ce82f75bc2b1b1523cccf4f9a812bfe9
john <126916249+johnSugden-vanDyk@users.noreply.github.com>
2024-06-27 10:10:21 -0600

    week 8 setup, & copy

---
+++ app/week-8/shopping-list/page.js
@@ -0,0 +1,50 @@
+"use client"
+import ItemList from "./item-list"
+import NewItem from "./new-item"
+import ItemsData from "./items.json"
+import MealIdeas from "./meal-ideas"
+
+import { useState } from "react"
+
+const Page = () => {
+
+    const [items, setItems] = useState(ItemsData);
+    const [selectedItemName, setSelectedItemName] = useState("");
+
+    const handleAddItem = (newItem) => {
+        setItems([...items, newItem]);
+    };
+
+    const handleItemSelected = (selectedItem) => {
+        let name = selectedItem.name;
+        let cleanedItemName = name.split(',')[0];
+        cleanedItemName = cleanedItemName.trim(); 
+        cleanedItemName = cleanedItemName.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '');
+
+        setSelectedItemName(cleanedItemName)
+    }
+
+
+    return (
+        <main className=" bg-violet-950">
+            <div className="p-3 text-violet-50" >
+            <h1 className="font-bold text-3xl py-2">Shopping list</h1>
+            <h2 className="text-xl">Add New Item</h2>
+            <div className="flex ">
+
+            <div className="flex-1 max-w-sm">
+                <NewItem onAddItem={handleAddItem}/>
+                <ItemList items={items} onItemSelect={handleItemSelected}/>
+            </div>
+            <div className="flex-1 max-w-sm">
+                <MealIdeas ingredient={selectedItemName}/>
+            </div>
+            </div>
+
+
+            </div>
+        </main>
+    )
+}
+
+export default Page
