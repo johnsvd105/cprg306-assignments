374a5b9cfc4f0d567158861e80d7aa3f44b084cd
john <126916249+johnSugden-vanDyk@users.noreply.github.com>
2024-06-30 14:46:45 -0600

    page.js for week 8 update
    added conditional rendering depending on if user is logged in or not, github login not working

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -1,15 +1,36 @@
+"use client"
 import { useUserAuth } from "./_utils/auth-context";
+
  
 
 
 
 const Page = () => {
-const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+    const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+    const handleGitHubSignIn = async () => {
+        try {
+            console.log("testing")
+            await gitHubSignIn();
+        } catch(error) {
+            console.error("Error singing in with github:", error)
+        }
+    }
+
+
 
     return (
-        <div>
-            <p>test</p>
-        </div>
+        <main className="bg-violet-950 flex-1">
+            <div>
+                {user ? (
+                    <p>Hello, {user.displayName}</p>
+                ) : (
+                    <div>
+                        <h1>Sign in with github</h1>
+                        <button onClick={handleGitHubSignIn} className="p-2"> Sign in</button>
+                    </div>
+                )}
+            </div>   
+        </main>
     )
 }
 
