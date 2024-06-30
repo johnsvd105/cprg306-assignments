01a8591fccd8f40e2bd0eb323eee49c47a245b0c
John <126916249+johnsvd105@users.noreply.github.com>
2024-06-30 14:57:14 -0600

    added sign out
    error with sign in wasn't code related, added log out and just a tiny bit of styling

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -9,10 +9,18 @@ const Page = () => {
     const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
     const handleGitHubSignIn = async () => {
         try {
-            console.log("testing")
             await gitHubSignIn();
         } catch(error) {
             console.error("Error singing in with github:", error)
+        }
+    }
+
+    const handleFirebaseSignOut = async () => {
+        try {
+            await firebaseSignOut();
+        } catch(error) {
+            //this shouldn't happen I believe, unless somehow someone tries to log out without being logged in
+            console.error("Error signing out:", error)
         }
     }
 
@@ -20,9 +28,12 @@ const Page = () => {
 
     return (
         <main className="bg-violet-950 flex-1">
-            <div>
+            <div className="p-3 text-violet-50">
                 {user ? (
-                    <p>Hello, {user.displayName}</p>
+                    <div>
+                    <h1> Hello, {user.displayName}</h1>
+                    <button onClick={handleFirebaseSignOut} className="p-2"> Sign out</button>
+                    </div>
                 ) : (
                     <div>
                         <h1>Sign in with github</h1>
